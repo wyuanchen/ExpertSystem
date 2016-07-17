55c9dfcd2d114073720dc9988a078b8559b17b3c
wyuanchen <yuan821491850@gmail.com>
2016-07-17 13:38:29 +0800

    增加专家审核界面

--- src/main/webapp/ui/js/common.js
+++ src/main/webapp/ui/js/common.js
@@ -2,23 +2,37 @@
  * Created by yuan on 16-7-12.
  */
 
+// 跳转页面
+// params为json对象
+function gotoUrl(url, params) {
+    if (params == null)
+        window.location.href = url;
+    url = url + "?";
+    for (var key in params) {
+        url = url + key + "=" + params[key] + "&";
+    }
+    if ("&" == url.charAt(url.length - 1))
+        url = url.substr(0, url.length - 1);
+    window.location.href = url;
+}
 
-function sendAjaxRequest(url,params,callBack){
+// 发送ajax请求
+function sendAjaxRequest(url, params, callBack) {
     $.ajax({
-        type:'POST',
-        dataType:'json',
-        url:url,
-        contentType:"application/json; charset=utf-8",
-        data:JSON.stringify(params),
+        type: 'POST',
+        dataType: 'json',
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(params),
         // data:params,
         xhrFields: {
             withCredentials: true
         },
         crossDomain: true,
-        success:function(data){
-            if(data!=null){
+        success: function(data) {
+            if (data != null) {
                 callBack(data);
-            }else{
+            } else {
                 //alert('error!');
             }
         },
@@ -35,26 +49,26 @@ function sendAjaxRequest(url,params,callBack){
 }
 
 
-function getUrlParams(){
-    var url=window.location.search;
-    var params=new Object();
-    if(url.indexOf('?')!=-1){
-        var str=url.substr(1);
-        strs=str.split('&');
-        for(var i=0;i<strs.length;i++){
-            var key=strs[i].split('=')[0];
-            var value=strs[i].split('=')[1];
-            params[key]=value;
+function getUrlParams() {
+    var url = window.location.search;
+    var params = new Object();
+    if (url.indexOf('?') != -1) {
+        var str = url.substr(1);
+        strs = str.split('&');
+        for (var i = 0; i < strs.length; i++) {
+            var key = strs[i].split('=')[0];
+            var value = strs[i].split('=')[1];
+            params[key] = value;
         }
     }
     return params;
 }
 
-function fillTemplate(template,jsonData){
-    return template.replace(/\$\w+\$/gi,function(matchs){
-        var value=jsonData[matchs.replace(/\$/g, "")];
-        return (value+"")=="undefined"?"":value;
+function fillTemplate(template, jsonData) {
+    return template.replace(/\$\w+\$/gi, function(matchs) {
+        var value = jsonData[matchs.replace(/\$/g, "")];
+        return (value + "") == "undefined" ? "" : value;
     });
 }
-var serverIP="http://localhost:8080/";
-var serverUrl=serverIP+"cinema/";
+var serverIP = "http://localhost:8080/";
+var serverUrl = serverIP + "cinema/";
--- src/main/webapp/ui/js/review.js
+++ src/main/webapp/ui/js/review.js
@@ -1,3 +1,58 @@
 /**
  * Created by yuan on 7/15/16.
  */
+var experts = [{
+    "expertCertificateId": 123123123,
+    "name": "yuan",
+    "workplace": "华南理工大学",
+    "telephone": 18826077178,
+    "registerState": "注册",
+    "status": "可用",
+}, {
+    "expertCertificateId": 123123123,
+    "name": "yuan",
+    "workplace": "华南理工大学",
+    "telephone": 18826077178,
+    "registerState": "注册",
+    "status": "可用",
+}, ];
+window.onload = function() {
+    showExpertList(experts);
+    setOnclikListener();
+}
+
+function showExpertList(experts) {
+    if (experts == null || experts.length < 1)
+        return;
+    var template = $("#template_tr_expert_info").html();
+    for(var i=0;i<experts.length;i++){
+      var newItem=fillTemplate(template,experts[i]);
+      $("#tbody_expert_list").append(newItem);
+    }
+}
+
+// 设置按钮点击监听者
+function setOnclikListener(){
+  // 设置下拉框里面的项目点击事件
+  $("body").on("click",".dropdown",setDropDownListResult);
+  // 设置搜索按钮的点击事件
+  $("body").on("click","#btn_search",searchExperts);
+}
+
+// 点击下拉框后更新相应输入框的值
+function setDropDownListResult() {
+    var value=$(this).text().trim();
+    $(this).parents(".input-group").children("input").prop("value",value);
+}
+
+//搜索满足一定条件的所有expert
+function searchExperts(){
+  var field=$("#field_filter").val();
+  var status=$("#status_filter").val();
+  var url="review.html";
+  var params={
+    "field":field,
+    "status":status,
+  }
+  gotoUrl(url,params);
+}
